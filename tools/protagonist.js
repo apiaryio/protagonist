fc98a9f47eb8442a26347313b9d5bf11bff42e5f
Z <z@apiary.io>
2015-02-04 10:31:30 +0100

    Address reviews comments

--- tools/protagonist.js
+++ tools/protagonist.js
@@ -8,8 +8,8 @@ var protagonist = require('../build/Release/protagonist');
 //var protagonist = require('protagonist');
 
 // Add Segfault handler
-// var segfaultHandler = require('segfault-handler');
-// segfaultHandler.registerHandler();
+var segfaultHandler = require('segfault-handler');
+segfaultHandler.registerHandler();
 
 // Process arguments
 var args = process.argv.slice(2);
