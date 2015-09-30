9450e776e1a0df02a0ef85232d61075c63fea00d
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2015-09-30 06:45:26 +0530

    Optimize Result::WrapResult

--- src/result.cc
+++ src/result.cc
@@ -1,6 +1,5 @@
 #include "protagonist.h"
-#include "SerializeAST.h"
-#include "SerializeSourcemap.h"
+#include "SerializeResult.h"
 #include "v8_wrapper.h"
 #include "snowcrash.h"
 
@@ -43,49 +42,22 @@ v8::Local<v8::Object> Result::WrapResult(const snowcrash::ParseResult<snowcrash:
                                          const snowcrash::BlueprintParserOptions& options,
                                          const drafter::ASTType& astType)
 {
-    Local<Function> cons = NanNew<Function>(constructor);
-    Local<Object> resultWrap = cons->NewInstance();
-
     static const char* AstKey = "ast";
-    static const char* WarningsKey = "warnings";
+    static const char* ErrorKey = "error";
     static const char* SourcemapKey = "sourcemap";
 
-    if (astType == drafter::RefractASTType) {
-      // The Refract output contains all the warnings and source map info, so
-      // we can just wrap and return the parse result.
-      sos::Object blueprintSerializationWrap = drafter::WrapParseResult(parseResult, options);
+    sos::Object result = drafter::WrapResult(parseResult, options, astType);
 
-      return v8_wrap(blueprintSerializationWrap)->ToObject();
+    if (parseResult.report.error.code != snowcrash::Error::OK) {
+        result.set(AstKey, sos::Null());
+
+        if ((options & snowcrash::ExportSourcemapOption) != 0) {
+            result.set(SourcemapKey, sos::Null());
+        }
+    }
+    else {
+        result.unset(ErrorKey);
     }
 
-    if (parseResult.report.error.code == snowcrash::Error::OK) {
-        sos::Object blueprintSerializationWrap = drafter::WrapBlueprint(parseResult.node, astType);
-
-        resultWrap->Set(NanNew<String>(AstKey), v8_wrap(blueprintSerializationWrap));
-    }
-    else
-        resultWrap->Set(NanNew<String>(AstKey), NanNull());
-
-    Local<Object> warnings = NanNew<Array>(parseResult.report.warnings.size());
-    size_t i = 0;
-
-    for (snowcrash::Warnings::const_iterator it = parseResult.report.warnings.begin();
-         it != parseResult.report.warnings.end();
-         ++it, ++i) {
-
-        warnings->Set(i, SourceAnnotation::WrapSourceAnnotation(*it));
-    }
-
-    resultWrap->Set(NanNew<String>(WarningsKey), warnings);
-
-    // Set source map only if requested
-    if (parseResult.report.error.code == snowcrash::Error::OK && (options & snowcrash::ExportSourcemapOption) != 0) {
-        sos::Object sourcemapSerializationWrap = drafter::WrapBlueprintSourcemap(parseResult.sourceMap);
-
-        resultWrap->Set(NanNew<String>(SourcemapKey), v8_wrap(sourcemapSerializationWrap));
-    }
-    else
-        resultWrap->Set(NanNew<String>(SourcemapKey), NanNull());
-
-    return resultWrap;
+    return v8_wrap(result)->ToObject();
 }
