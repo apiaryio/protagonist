4188e06fa8a8f3a33073b96c44518a510a93cf83
Kyle Fuller <kyle@fuller.li>
2019-06-14 18:45:47 +0100

    refactor: align with nan example

--- src/protagonist.cc
+++ src/protagonist.cc
@@ -3,19 +3,18 @@
 using namespace v8;
 using namespace protagonist;
 
-void Init(Local<Object> exports) {
-    Local<Context> context = Nan::GetCurrentContext();
+NAN_MODULE_INIT(InitAll) {
+    Nan::Set(target, Nan::New<String>("parse").ToLocalChecked(),
+        Nan::GetFunction(Nan::New<FunctionTemplate>(Parse)).ToLocalChecked());
 
-    // parse function
-    exports->Set(context, Nan::New<String>("parse").ToLocalChecked(), Nan::New<FunctionTemplate>(Parse)->GetFunction(context).ToLocalChecked());
+    Nan::Set(target, Nan::New<String>("parseSync").ToLocalChecked(),
+        Nan::GetFunction(Nan::New<FunctionTemplate>(ParseSync)).ToLocalChecked());
 
-    // parseSync function
-    exports->Set(context, Nan::New<String>("parseSync").ToLocalChecked(), Nan::New<FunctionTemplate>(ParseSync)->GetFunction(context).ToLocalChecked());
+    Nan::Set(target, Nan::New<String>("validate").ToLocalChecked(),
+        Nan::GetFunction(Nan::New<FunctionTemplate>(Validate)).ToLocalChecked());
 
-    // validate function
-    exports->Set(context, Nan::New<String>("validate").ToLocalChecked(), Nan::New<FunctionTemplate>(Validate)->GetFunction(context).ToLocalChecked());
-    // validateSync function
-    exports->Set(context, Nan::New<String>("validateSync").ToLocalChecked(), Nan::New<FunctionTemplate>(ValidateSync)->GetFunction(context).ToLocalChecked());
+    Nan::Set(target, Nan::New<String>("validateSync").ToLocalChecked(),
+        Nan::GetFunction(Nan::New<FunctionTemplate>(ValidateSync)).ToLocalChecked());
 }
 
-NODE_MODULE(protagonist, Init)
+NODE_MODULE(protagonist, InitAll)
